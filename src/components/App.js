1b076be2b9b359f76ac6aa6ae8eee4a796027f76
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-05 21:32:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { createContext, useContext } from 'react'
+
+
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
-
+const UserContext = createContext();
 const App = () => {
+
+
+  let data = { name: "Newton", age: 3 };
 
   return (
     <div id="main">
-      
+      <UserContext.Provider value={{ name: "Newton", age: 3 }}>
         <UserProfile />
-      
+
+      </UserContext.Provider>
+
     </div>
   )
 }
 
 
 export default App;
-export {UserContext}
+export { UserContext };
