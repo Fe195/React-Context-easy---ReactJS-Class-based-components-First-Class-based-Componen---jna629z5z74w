20ee3f2b2341dc1f357d170fee7e655deca236dc
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-05 21:33:40 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,19 @@
-import { useContext } from 'react'
-import React from 'react'
-import { UserContext } from "./App"
+import React from 'react';
+import { useContext } from 'react';
 
-const UserProfile = () =>{
+import { UserContext } from "./App";
+
+const UserProfile = () => {
+    const detail = useContext(UserContext);
+    console.log(detail);
+
+    return (
+        <>
+            <h1 id='name'>Name:- {detail.name}</h1>
+            <h1 id='age'>Age:- {detail.age}</h1>
+        </>
+    )
 
 }
 
-export {UserProfile}
+export { UserProfile }
